f8bfdd8ea190ea96ec24e1a35cf17e5409e31a88
arnabbis <biswas.anuska.2004@gmail.com>
2022-02-22 21:03:07 +0530

    Nodejs module system

--- src/Question no.4/chunk.js
+++ src/Question no.4/chunk.js
@@ -1,4 +1,4 @@
-let _ = require("lodash");
+let _= require("lodash");
 let arrayMonth = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 const result =  _.chunk(arrayMonth, 3);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,14 @@
 let obj1 = require("../logger/logger.js");
 let obj2 = require("../util/helper.js");
-let obj3 = require("../question4/chunk.js");
-let obj4 = require("../question4/tail.js");
-let obj5 = require("../question4/union");
-let obj6 = require("../question4/frompair.js");
+let obj3 = require("../Question no.4/chunk");
+let obj4 = require("../Question no.4/tail");
+let obj5 = require("../Question no.4/union");
+let obj6 = require("../Question no.4/frompair");
 const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    obj1.welcome("Welcome to my application. I am Aman Pandey and a part of FunctionUp Thorium cohort.");
+    obj1.welcome("Welcome to my application. I am Arnab Biswas and a part of FunctionUp Thorium cohort.");
     obj2.printDate("Today is 21st feb");
     obj2.printMonth("Month is February");
     obj2.getBatchInfo("Thorium, W3D1, the topic for today is Nodejs module system");
@@ -22,6 +22,6 @@ router.get('/hello', function (req, res){
     console.log(obj4.myArray);
     console.log(obj5.myArray2);
     console.log(obj6.myArray3);
-    res.send('Question 4 succesfully executed!!');
+    res.send('Question no.4 succesfully executed!!');
 });
 module.exports = router;
