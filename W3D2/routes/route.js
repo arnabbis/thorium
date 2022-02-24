b2757c76d74ea9ea3b0cbfb8b1dc3f587b046980
arnabbis <biswas.anuska.2004@gmail.com>
2022-02-24 21:22:13 +0530

    code2

--- W3D2/routes/route.js
+++ W3D2/routes/route.js
@@ -1,72 +1,49 @@
 const express = require('express');
 const router = express.Router();
+let players = [];
 
+router.post('/players', function(req, res) {
 
-let arr =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ],
-           "bookings": [
-               {
-                   "bookingNumber": 1,
-                   "sportId": "",
-                   "centerId": "",
-                   "type": "private",
-                   "slot": '16286598000000',
-                   "bookedOn": '31/08/2021',
-                   "bookedFor": '01/09/2021'
-               },
-               {
-                   "bookingNumber": 2,
-                   "sportId": "",
-                   "centerId": "",
-                   "type": "private",
-                   "slot": '16286518000000',
-                   "bookedOn": '31/08/2001',
-                   "bookedFor": '01/09/2001'
-               },
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ],
-           "bookings": []
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ],
-           "bookings": []
-       },
-   ]
-   router.post('/player', function (req, res) {
-    let details = req.body.name1.name
-    let inputDetails = req.body.name1
-    for (let i = 0; i < arr.length; i++) {
-    if (details === arr[i].name) {
-    console.log("Data already exist")
-    res.send("Data already exist")
+    let player = req.body;
+    let playerName = player.name
+    for(let i = 0; i < players.length; i++){
+        if(players[i].name == playerName){
+            res.send('player already exist')
+        }
     }
-    else if (i === arr.length - 1) {
-    arr.push( inputDetails )
-    res.send({arr})
-    } 
-    }  
-    res.send(  { data: players , status: true }  )
-   })
-   module.exports=router;
+    players.push(player);
+    console.log('here is the playter array',players);
+    res.send(players);
+});
+
+router.post('/players/:playerName/bookings/:bookingId', function(req, res){
+    let name = req.params.playerName
+    let isPlayerPresent = false
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == name) {
+        isPlayerPresent = true
+        }
+    }
+
+    if(! isPlayerPresent){
+        return res.send('player not present')
+    }
+
+    let booking = req.body
+    let bookindId = req.params.bookingId
+    for(let i = 0; i < players.length; i++){
+        if(players[i].name == name) {
+            let isBookingPresent = false
+            for(let j = 0; j < players[i].bookings.length; j++){
+                if(players[i].bookings[j])
+            
+            players[i].bookings.push(booking)
+        }
+    }
+}
+
+    res.send(players)
+
+});
+module.exports = router;
