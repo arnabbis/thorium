664dfc90db5b565b05978b77a82041f10737061c
arnabbis <biswas.anuska.2004@gmail.com>
2022-02-24 10:28:48 +0530

    code1

--- W3D2/routes/route.js
+++ W3D2/routes/route.js
@@ -1,42 +1,72 @@
 const express = require('express');
 const router = express.Router();
-router.get('/test-me', function (req, res) {
-    res.send('Welcome to my movies collections!');
-});
-// 1. THIS API WILL SHOW ALL MOVIES IN ARRAY
-router.get('/MOVIES', function (req, res){
-   res.send('["darbar","sooryavanshi","1983","hum sath sath hain","hum"]')
-});
-//2. THIS API WILLfetch all MOVIES by index id 
-router.get('/MOVIES/:movieid', function (req, res){
-mov = ["darbar","sooryavanshi","1983","hum sath sath hain","hum"]
-let value = req.params.movieid;
-if (value>mov.length-1){
-    res.send('"doesnot exist"')
-} else{
-    res.send(mov[value])
-}
-})
-// 3. this api will fetch all movies from array all objects
-router.get('/MOVIEz', function (req, res){
-
-    res.send([ {id: 1,name: 'The Shining'}, {id: 2,name: 'Incendies'},{id: 3,name: 'Rang de Basanti'},{id: 4,name:'Finding Demo'},{id: 5,name: 'hum'}]);
-});
-// 4. this api will fetch all movies from array of objects by Indexid
-router.get('/films/:flimId', function (req, res){
-let movi=[{id: 1,name: 'The Shining'}, {id: 2,name: 'Incendies'},{id: 3,name: 'Rang de Basanti'},{id: 4,name:'Finding Demo'},{id: 5,name: 'hum'}]
-let value = req.params.flimId;
-let found = false;
-for (i=0;i< movi.length;i++){
-if (movi[i].id == value){
-    found = true
-    res.send(movi[i])
-    break
-}
-}
-if (found==false){
-    res.send('no movie exist with this id')}
-});
 
 
-module.exports = router;
+let arr =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ],
+           "bookings": [
+               {
+                   "bookingNumber": 1,
+                   "sportId": "",
+                   "centerId": "",
+                   "type": "private",
+                   "slot": '16286598000000',
+                   "bookedOn": '31/08/2021',
+                   "bookedFor": '01/09/2021'
+               },
+               {
+                   "bookingNumber": 2,
+                   "sportId": "",
+                   "centerId": "",
+                   "type": "private",
+                   "slot": '16286518000000',
+                   "bookedOn": '31/08/2001',
+                   "bookedFor": '01/09/2001'
+               },
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+           "bookings": []
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+           "bookings": []
+       },
+   ]
+   router.post('/player', function (req, res) {
+    let details = req.body.name1.name
+    let inputDetails = req.body.name1
+    for (let i = 0; i < arr.length; i++) {
+    if (details === arr[i].name) {
+    console.log("Data already exist")
+    res.send("Data already exist")
+    }
+    else if (i === arr.length - 1) {
+    arr.push( inputDetails )
+    res.send({arr})
+    } 
+    }  
+    res.send(  { data: players , status: true }  )
+   })
+   module.exports=router;
