b99528c83472c5536b698f64ea81956c2259aa74
arnabbis <biswas.anuska.2004@gmail.com>
2022-03-01 18:14:05 +0530

    mongosse2

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,24 +1,61 @@
-const { count } = require("console")
-const bookModel= require("../models/bookModel")
+const BookModel= require("../models/bookModel")
 
-const createBook = async function(req, res){
-    let bookData = req.body
-    let save =  await bookModel.create(bookData);
-    console.log('stored book data successfully');
-    res.send({ "Book Data" : save});
+const createBook= async function (req, res) {
+    let data= req.body
+
+    let savedData= await BookModel.create(data)
+    res.send({msg: savedData})
 }
-const bookList = async function(req ,res){
-    let booksName = await bookModel.find().select({bookName : 1, authorName : 1});
-    console.log('Data sent successfully');
-    res.send({"List of books and authors " : booksName})
+
+
+const getBookList= async function (req, res) {
+    let allUsers= await BookModel.find().select({bookName:1,authorName:1,_id:0})
+    res.send({msg: allUsers})
 }
-const getBooksInYear = async function(req, res){
-    let bookYearData = req.body.year
-    let bookYear = await bookModel.find({year : bookYearData});
-    console.log('Data sent successfully');
-    res.send({ "Book Data according to the year" : bookYear});
+
+
+const booksYear= async function (req, res) {
+    let yer=req.query.yer
+    let allUsers= await BookModel.find({year:{$eq:yer}})
+    res.send({msg: allUsers})
 }
- 
-module.exports.createBook = createBook;
-module.exports.bookList = bookList;
-module.exports.getBooksInYear = getBooksInYear;
+
+
+const getPaticularBooks= async function (req, res) {
+    let name=req.query.name
+    let year=req.query.year
+    let aname=req.query.aname
+    let totalpage=req.query.totalpage
+
+    let allUsers= await BookModel.find({
+        $or:[{authorName:{$eq:aname}},{year:{$eq:year}},{bookName:{$eq:name}},{totalPages:{$eq:totalpage}}]
+    }).select()
+    res.send({msg: allUsers})
+}
+
+
+const getInrBooks= async function (req, res) {
+    let yer=req.query.yer
+    let allUsers= await BookModel.find({
+        $or:[
+            {"prices.indianPrice":"100"},
+            {"prices.indianPrice":"200"},
+            {"prices.indianPrice":"500"},
+        ]
+    })
+    res.send({msg: allUsers})
+}
+
+
+const getRandomBooks= async function (req, res) {
+    let allUsers= await BookModel.find({stockAvailable:true,totalPages:{$gt:500}})
+    res.send({msg: allUsers})
+}
+
+
+module.exports.createBook= createBook
+module.exports.getBookList= getBookList
+module.exports.booksYear=booksYear
+module.exports.getPaticularBooks=getPaticularBooks
+module.exports.getInrBooks=getInrBooks
+module.exports.getRandomBooks=getRandomBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const BookController = require("../controllers/bookController.js")
 
-router.post("/createBook", BookController.createBook);
-router.get("/bookList", BookController.bookList);
-router.post("/getBooksInYear",BookController.getBooksInYear);
-// router.post("/getParticularBooks",BookController.getParticularBooks)
-// router.get("/getXINRBooks", BookController.getXINRBooks);
-  
+const UserController= require("../controllers/userController")
+const BookController= require("../controllers/bookController")
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+
+
+
+router.post("/createBook", BookController.createBook  )
+
+router.get("/bookList", BookController.getBookList)
+
+router.post("/booksInYear",BookController.booksYear)
+
+router.get("/paticularBooks",BookController.getPaticularBooks)
+
+router.get("/INRBooks", BookController.getInrBooks)
+
+router.get("/randomBooks",BookController.getRandomBooks)
+
 module.exports = router;
