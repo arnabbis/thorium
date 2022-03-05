60de840f839bb350b290477914d97d460c3b5384
arnabbis <biswas.anuska.2004@gmail.com>
2022-03-06 01:30:14 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const req = require('express/lib/request');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    function(req,res,next){
+    let date = new Date()
+    let ownip=req.socket.remoteAddress
+    console.log(date,ownip,req.originalUrl)
+    next()
+    }
+)
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://arnabbiswas_14:arnabbiswas@cluster0.b95gv.mongodb.net/arnab-1234?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,16 +8,16 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.get("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+router.get("/createBook", BookController.createBook  )
 
 router.get("/getBooksData", BookController.getBooksData)
 
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
+router.get("/updateBooks", BookController.updateBooks)
+router.get("/deleteBooks", BookController.deleteBooks)
 
 //MOMENT JS
 const moment = require('moment');
