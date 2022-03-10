4e9ecd5d971454838b5c2362801970b2d243c232
arnabbis <biswas.anuska.2004@gmail.com>
2022-03-10 23:48:31 +0530

    http

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,29 +1,43 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 const authenticate =async function(req, res, next) {
+  try{
     //check the token in request header
     //validate this token
           let token = req.headers["x-auth-token"];
         //   if (!token) token = req.headers["x-auth-token"];
-          if (!token) return res.send({ status: false, msg: "token must be present" });
+          if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
       
           let decodedToken = jwt.verify(token, "functionup-thorium");
         //   console.log(decodedToken)
-          if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
+          if (!decodedToken) return res.status(400).send({ status: false, msg: "token is invalid" });
           next()
+      }catch (err) {
+        console.log("This is the error", err.message)
+        res.status(500).send({ msg: "Error", error:"arnabbiswas" });
       }
+    };
 
 
 
 
 const authorise = async function(req, res, next) {
+  try{
     // comapre the logged in user's id and the id in request
         let userToBeModified = req.params.userId
         let token = req.headers["x-auth-token"]
         let decodedToken = jwt.verify(token, "functionup-thorium")
         let userLoggedIn = decodedToken.userId
-        if (userToBeModified != userLoggedIn) return res.send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
+        if (userToBeModified != userLoggedIn) return res.status(400).send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
         next()
+      }catch (err) {
+        console.log("This is the error", err.message)
+        res.status(500).send({ msg: "Error", error:"arnabbiswas" });
       }
+    };
+
+
+
+
 module.exports.authenticate = authenticate;
 module.exports.authorise = authorise;
